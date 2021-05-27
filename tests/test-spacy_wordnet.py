8b05ce22d49e2eee999f30033005a888f696fdd5
Milos Rancic <millosh@gmail.com>
2021-05-27 13:38:53 +0200

    .

---
+++ tests/test-spacy_wordnet.py
@@ -0,0 +1,15 @@
+import spacy
+from spacy_wordnet.wordnet_annotator import WordnetAnnotator 
+
+# Load an spacy model (supported models are "es" and "en") 
+nlp = spacy.load('en')
+nlp.add_pipe(WordnetAnnotator(nlp.lang), after='tagger')
+token = nlp('prices')[0]
+
+# wordnet object link spacy token with nltk wordnet interface by giving acces to
+# synsets and lemmas 
+print(token._.wordnet.synsets())
+print(token._.wordnet.lemmas())
+
+# And automatically tags with wordnet domains
+print(token._.wordnet.wordnet_domains())
