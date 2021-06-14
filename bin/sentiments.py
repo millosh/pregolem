74f961a81df966cd1b5fc35c1a83b02c7323c169
Milos Rancic <millosh@gmail.com>
2021-06-14 21:07:15 +0200

    debugging

--- bin/sentiments.py
+++ bin/sentiments.py
@@ -265,7 +265,7 @@ def make_domains(working_entity,args):
             domains[domain] += 1
     return domains
 
-def make_sentiments(working_entity,token,others,args,data):
+def make_sentiments(working_entity,token,others,doc,args,data):
     lemma = token.lemma_
     token_id = token.i
     if lemma not in data['sentiments']:
@@ -285,20 +285,30 @@ def make_sentiments(working_entity,token,others,args,data):
             data['sentiments'][lemma]['positivity score'] += working_word['sentiments']['positivity score'] * float(1)/float(wmax)
             data['sentiments'][lemma]['objectivity score'] += working_word['sentiments']['objectivity score'] * float(1)/float(wmax)
             data['sentiments'][lemma]['sentiment addition frequency'] += 1
-            for other in others:
-                #if others[other]['working entity']['token id'] != token_id:
-                other_lemma = others[other]['working entity']['lemma']
-                if other_lemma not in data['sentiments']:
-                    data['sentiments'][other_lemma] = {
-                        'negativity score': 0,
-                        'positivity score': 0,
-                        'objectivity score': 0,
-                        'sentiment addition frequency': 0,
-                    }
-                    data['sentiments'][other_lemma]['negativity score'] += working_word['sentiments']['negativity score'] * float(1)/float(len(others))
-                    data['sentiments'][other_lemma]['positivity score'] += working_word['sentiments']['positivity score'] * float(1)/float(len(others))
-                    data['sentiments'][other_lemma]['objectivity score'] += working_word['sentiments']['objectivity score'] * float(1)/float(len(others))
-                    data['sentiments'][other_lemma]['sentiment addition frequency'] += 1
+            #for other in others:
+            #    if others[other]['working entity']['token id'] != token_id:
+            #        other_lemma = others[other]['working entity']['lemma']
+            #        if other_lemma not in data['sentiments']:
+            #            data['sentiments'][other_lemma] = {
+            #                'negativity score': 0,
+            #                'positivity score': 0,
+            #                'objectivity score': 0,
+            #                'sentiment addition frequency': 0,
+            #            }
+            for otoken in doc:
+                if otoken.i != token_id:
+                    other_lemma = otoken.lemma_
+                    if other_lemma not in data['sentiments']:
+                        data['sentiments'][other_lemma] = {
+                            'negativity score': 0,
+                            'positivity score': 0,
+                            'objectivity score': 0,
+                            'sentiment addition frequency': 0,
+                        }
+                        data['sentiments'][other_lemma]['negativity score'] += working_word['sentiments']['negativity score'] * float(1)/float(len(others))
+                        data['sentiments'][other_lemma]['positivity score'] += working_word['sentiments']['positivity score'] * float(1)/float(len(others))
+                        data['sentiments'][other_lemma]['objectivity score'] += working_word['sentiments']['objectivity score'] * float(1)/float(len(others))
+                        data['sentiments'][other_lemma]['sentiment addition frequency'] += 1
     return data
 
 def update_paragraphs(paragraphs,args,data):
@@ -360,7 +370,7 @@ def update_paragraphs(paragraphs,args,data):
                                 data['paragraphs'][pkey]['sentences'][skey]['domains'][domain] += data['paragraphs'][pkey]['sentences'][skey]['tokens'][tkey]['domains'][domain]
                         elif args['command'] == "make-sentiments":
                             tokens = paragraphs[pkey]['sentences'][skey]['tokens']
-                            data = make_sentiments(working_entity,token,tokens,args,data)
+                            data = make_sentiments(working_entity,token,tokens,doc,args,data)
                         paragraphs[pkey]['sentences'][skey]['tokens'][tkey]['working entity'] = working_entity
                     if args['command'] == "make-domains":
                         for domain in data['paragraphs'][pkey]['sentences'][skey]['domains']:
