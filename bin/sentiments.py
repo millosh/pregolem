f3752db3839978c38baed76d26ae3ac625d1cf5f
Milos Rancic <millosh@gmail.com>
2021-07-08 20:49:47 +0200

    domains

--- bin/sentiments.py
+++ bin/sentiments.py
@@ -33,7 +33,7 @@ def get_args():
     args = {}
     data = {
         'domains': {},
-        'sepcific domains': {},
+        'specific domains': {},
         'sentiments': {},
         'paragraphs': {},
     }
