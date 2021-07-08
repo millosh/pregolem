b4259c7d630a1f9692757407c32b29700c4f4b02
Milos Rancic <millosh@gmail.com>
2021-07-08 21:22:57 +0200

    domains

--- bin/sentiments.py
+++ bin/sentiments.py
@@ -351,29 +351,29 @@ def make_domain(working_entity,token,others,doc,args,data):
                     data['specific domains'][args['domain-name']][lemma][domain] = 0
                 data['specific domains'][args['domain-name']][lemma][domain] = (data['specific domains'][args['domain-name']][lemma][domain] + 1)/2
     
-    # if lemma not in data['sentiments']:
-    #     data['sentiments'][lemma] = {
-    #         'negativity score': 0,
-    #         'positivity score': 0,
-    #         'objectivity score': 0,
-    #         'sentiment addition frequency': 0,
-    #     }
-    working_words = working_entity['working words']
-    wmin = 0
-    wmax = len(working_words)
-    for w in range(wmin,wmax):
-        working_word = working_words[w]
-        for form in domain_dict:
-            data['specific domains'][args['domain-name']][lemma][domain] += float(1)/float(wmax)
-            for otoken in doc:
-                if otoken.i != token_id:
-                    other_lemma = otoken.lemma_
-                    other_lower = otoken.lower_
-                    if other_lemma not in data['specific domains'][args['domain-name']]:
-                        data['specific domains'][args['domain-name']][other_lemma] = {}
-                    if domain not in data['specific domains'][args['domain-name']][other_lemma]:
-                        data['specific domains'][args['domain-name']][other_lemma][domain] = 0
-                    data['specific domains'][args['domain-name']][other_lemma][domain] = (data['specific domains'][args['domain-name']][lemma][domain] + 1)/2
+            # if lemma not in data['sentiments']:
+            #     data['sentiments'][lemma] = {
+            #         'negativity score': 0,
+            #         'positivity score': 0,
+            #         'objectivity score': 0,
+            #         'sentiment addition frequency': 0,
+            #     }
+            working_words = working_entity['working words']
+            wmin = 0
+            wmax = len(working_words)
+            for w in range(wmin,wmax):
+                working_word = working_words[w]
+                for form in domain_dict:
+                    data['specific domains'][args['domain-name']][lemma][domain] += float(1)/float(wmax)
+                    for otoken in doc:
+                        if otoken.i != token_id:
+                            other_lemma = otoken.lemma_
+                            other_lower = otoken.lower_
+                            if other_lemma not in data['specific domains'][args['domain-name']]:
+                                data['specific domains'][args['domain-name']][other_lemma] = {}
+                            if domain not in data['specific domains'][args['domain-name']][other_lemma]:
+                                data['specific domains'][args['domain-name']][other_lemma][domain] = 0
+                                data['specific domains'][args['domain-name']][other_lemma][domain] = (data['specific domains'][args['domain-name']][lemma][domain] + 1)/2
     return args, data
 
 def update_paragraphs(paragraphs,args,data):
