628c2a36cf7c1068189606f95d1fe8d5064c2962
Milos Rancic <millosh@gmail.com>
2021-07-08 20:52:00 +0200

    domains

--- bin/sentiments.py
+++ bin/sentiments.py
@@ -341,7 +341,7 @@ def make_domain(working_entity,token,others,doc,args,data):
     #    sys.exit()
     print(data.keys())
     if args['domain-name'] not in data['specific domains']:
-        args['specific domains'][args['domain-name']] = {}
+        data['specific domains'][args['domain-name']] = {}
     for form in domain_dict:
         if re.search(form,lower):
             domain = domain_dict[form]
