8bc227cea9f393669a7b35ecccffe32f2067f5f3
Milos Rancic <millosh@gmail.com>
2022-03-07 17:34:11 +0100

    .

--- bin/semantic_analysis.py
+++ bin/semantic_analysis.py
@@ -204,9 +204,9 @@ def process_text(args):
 def update_paragraphs(paragraphs,args,data):
     p = 0
     nrel = 0
-    start = 385
+    start = 386
     stop = len(paragraphs)
-    stop = 386
+    stop = 387
     for pkey in paragraphs:
         if p <= start:
             next
